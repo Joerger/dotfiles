a868bea5d5041686562ed1ad69ebef61de76b919
joerger <bjoerger@goteleport.com>
2022-03-16 16:37:44 -0700

    Add git and kitty install.

---
+++ kitty.sh
@@ -0,0 +1,16 @@
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+
+# Create a symbolic link to add kitty to PATH (assuming ~/.local/bin is in
+# your PATH)
+ln -s ~/.local/kitty.app/bin/kitty ~/.tools/bin/
+# Place the kitty.desktop file somewhere it can be found by the OS
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+# Update the path to the kitty icon in the kitty.desktop file(s)
+sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+# You may need to add the full kitty path to the kitty.desktop file manually
+
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator `which kitty` 50
+
+
